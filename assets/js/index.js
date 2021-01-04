46a437ac38564be404c74b7e0f803497b7799594
richard-geddis <richardgeddis8@gmail.com>
2021-01-04 19:47:34 +0000

    updating the pictures

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,20 @@
 const driver = ["Lewis Hamilton", "George Russell", "Max Verstappen", "", "Fernando Alonso"];
-let driversCount = 0;
+let driverCount = 0;
 let position = 0;
-const drivers = ["Lewis Hamilton", "George Russell", "Max Verstappen", "", "Fernando Alonso"];
+const grid = ["Lewis Hamilton", "George Russell", "Max Verstappen", "", "Fernando Alonso"];
 
-function shuffleArray(drivers) {
-    for (let index = drivers.length - 1; index > 0; index--) {
-        let driverIndex = Math.floor(Math.random() * (index + 1));
-        [drivers[index], drivers[driverIndex]] = [drivers[driverIndex], drivers[index]];
+function shuffleArray(grid) {
+    for (let index = grid.length - 1; index > 0; index--) {
+        let gridIndex = Math.floor(Math.random() * (index + 1));
+        [grid[index], grid[gridIndex]] = [grid[gridIndex], grid[index]];
     }
 }
-
 function resetAnswers() {
     let shuffledArray = [];
     do {
-        shuffleArray(drivers);
-        shuffledArray = [drivers[0], drivers[1], drivers[2]];
-    } while (shuffledArray.includes(driver[driversCount]));
+        shuffleArray(grid);
+        shuffledArray = [grid[0], grid[1], grid[2]];
+    } while (shuffledArray.includes(driver[driverCount]));
     shuffledArray.push(driver[driverCount]);
     shuffleArray(shuffledArray);
     $(".answer-driver-a").text(shuffledArray[0]);
@@ -27,16 +26,16 @@ function resetAnswers() {
 function renderNextQuestion() {
     resetAnswers();
     resetButtons();
-    nextFlag();
+    nextDriver();
     endGame();
 }
 
-function evaluateAnswer(incrementScore) {
+function evaluateAnswer(incrementPosition) {
     setTimeout(function () {
-        if (incrementScore === true) {
-            score++;
+        if (incrementPosition === true) {
+            position++;
         }
-        driversCount++;
+        driverCount++;
         changeCounters();
         renderNextQuestion();
     }, 2000);
@@ -44,7 +43,7 @@ function evaluateAnswer(incrementScore) {
 
 function startGame() {
     $(".answer-button").attr("disabled", true);
-    let answer = driver[driversCount];
+    let answer = driver[driverCount];
     if ($(this).text().match(answer)) {
         $(this).css("background-color", "green");
         evaluateAnswer(true);
@@ -57,7 +56,7 @@ function startGame() {
 }
 
 function changeCounters() {
-    $(".flag-count").text("driver: " + driversCount + "/" + driver.length);
+    $(".driver-count").text("driver: " + driverCount + "/" + driver.length);
     $(".position").text("position: " + position);
 }
 
@@ -71,13 +70,13 @@ function resetButtons() {
 
 function endGame() {
     if (driversCount === driver.length) {
-        $(".driver").attr("src", "assets/images/" + flags[0] + "George Russel.jpg");
-        if (score < flags.length / 2) {
-            $(".end-modal-title").text("You Lost!");
+        $(".driver").attr("src", "assets/images/" + driver[0] + "George Russel.jpg");
+        if (position < driver.length / 2) {
+            $(".end-title").text("You Lost!");
         } else {
             $(".end-modal-title").text("Congratulations you know your F1 Drivers!");
         }
-        $(".end-modal-score").text("position: " + position + " /" +
+        $(".end-position").text("position: " + position + " /" +
             driver.length);
         $("#endModal").modal("show");
     }
